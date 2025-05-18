291528efb3d9405e299aea5448f3b343718e1324
prafullofcode <dahal.praphul268@gmail.com>
2025-05-18 07:33:18 +0545

    commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const shareButton = document.querySelector(".share-container");
+const activeStateShareContainer = document.querySelector(
+  ".active-state-share-popup"
+);
+const mobileActiveStateShareContainer = document.querySelector(
+  ".active-state-mobile"
+);
+const footer = document.querySelector(".content-footer");
+const author = document.querySelector(".author");
+const shareContainer = document.querySelector(".share-container");
+
+shareButton.addEventListener("click", function () {
+  activeStateShareContainer.classList.toggle("hidden");
+
+  if (window.innerWidth <= 800) {
+    author.classList.toggle("hidden");
+    shareContainer.classList.toggle("share-container-mobile");
+    mobileActiveStateShareContainer.classList.toggle("hidden");
+    footer.classList.toggle("content-footer-active");
+  }
+});
